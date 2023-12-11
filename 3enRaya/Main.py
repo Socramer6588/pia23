c08f2917a4732b57acde2f77df58527ef798a897
Marcos Domínguez Ordóñez <marcos6588@gmail.com>
2023-12-11 20:09:41 +0100

    Update 3enRaya

--- 3enRaya/Main.py
+++ 3enRaya/Main.py
@@ -1,6 +1,10 @@
+import random
 
+HUMAN = "O"
+MACHINE = "X"
+board = ["1","2","3","4","X","6","7","8","9"]
 
-def display_board(board):
+def display_board():
     print("+-------+-------+-------+")
     print("|       |       |       |")
     print("|   " + board[0] + "   |   " + board[1] + "   |   " + board[2] + "   |")
@@ -16,7 +20,7 @@ def display_board(board):
     print("+-------+-------+-------+")
     
 
-def check_winner(board, player):
+def check_winner(player):
     return ((board[0] == board[1] == board[2] == player) or
             (board[3] == board[4] == board[5] == player) or
             (board[6] == board[7] == board[8] == player) or
@@ -25,19 +29,52 @@ def check_winner(board, player):
             (board[2] == board[5] == board[8] == player) or
             (board[0] == board[4] == board[8] == player) or
             (board[2] == board[4] == board[6] == player))
-
-def is_board_full(board):
+    
+def is_board_full():
     return len(board) == board.count('X') + board.count('O')
 
+def play_machine():
+    zone = random.randint(1,9)
+    if board[zone-1] == "X" or board[zone-1] == "O":
+        play_machine()
+    board[zone-1] = MACHINE
+
+def play_game():
+    display_board()
+    print("Introduce una casilla para jugar")
+    zone = int(input())
+
+    if zone < 1 or zone > 9:
+        print("Número incorrecto, inténtalo de nuevo")
+        play_game()
+
+    if board[zone-1] == "X" or board[zone-1] == "O":
+        print("La casilla ya está ocupada, inténtalo de nuevo")
+        play_game()
+
+    board[zone-1] = HUMAN
+
+    if check_winner(HUMAN) == True:
+        display_board()
+        print("Has ganado!")
+        return
+    
+    play_machine()
+
+    if check_winner(MACHINE) == True:
+        display_board()
+        print("Has perdido!")
+        return
+    
+    if is_board_full == True:
+        display_board()
+        print("Empate!")
+        return
+    play_game()
+
 def main():
-    HUMAN = "O"
-    MACHINE = "X"
-    board = ["1","2","3","4","X","5","6","7","8","9"]
-
     print("Bienvenido a 3 en raya:")
-    display_board(board)
-    print(check_winner(board,HUMAN))
-
-
+    play_game()
+  
 if __name__ == "__main__":
     main()
